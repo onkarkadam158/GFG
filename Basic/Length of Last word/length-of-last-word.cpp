8892a9a5ed19d19d6e14af0aa9280b0815d49d8d
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-02-22 12:06:15 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Length of Last word/length-of-last-word.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+// CPP program for implementation of efficient 
+// approach to find length of last word 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int findLength(string s);
+
+// Driver code 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	t+=1;
+	bool flag = false;
+	while(t--){
+	    string s;
+	    getline(cin,s);
+	    if(!flag){
+	        flag = true;
+	        continue;
+	    }
+	    cout<<findLength(s)<<endl;
+	}
+	return 0;
+} 
+
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+int findLength(string s){
+    //code
+    int cnt=0,res;
+    for(int i=0;i<s.size();i++){
+        if(s[i]==' '){
+            cnt=0;
+        }
+        else{
+            cnt++;
+            res=cnt;
+        }
+    }
+    return res;
+}

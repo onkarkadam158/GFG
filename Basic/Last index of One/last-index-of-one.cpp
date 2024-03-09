785403db025f9eecc2fa35528c3a52c9fc9a970c
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-03-09 19:06:08 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Last index of One/last-index-of-one.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int lastIndex(string s) 
+    {   int res=-1;
+        for(int ind=0;ind<s.size();ind++){
+            if(s[ind]=='1') res=ind;
+        }
+        return res;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.lastIndex(s) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

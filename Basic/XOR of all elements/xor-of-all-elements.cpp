0f08fa2d5bbc01c9650a18aac29af1e2e6e0d292
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-03-09 19:38:18 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/XOR of all elements/xor-of-all-elements.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    vector<int> getXor(vector<int> A, int N)
+    {
+        // Write your Code here
+        int res=A[0];
+        for(int i=1;i<N;i++){
+            res=res xor A[i];
+        }
+        for(int i=0;i<N;i++){
+            A[i]=res xor A[i];
+        }
+        return A;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N ;
+        vector<int> A(N);
+
+        for (int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
+
+        Solution ob;
+        vector<int> B = ob.getXor(A, N);
+        for(int i = 0 ; i < N ; i++)
+        {
+            cout << B[i] <<" ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

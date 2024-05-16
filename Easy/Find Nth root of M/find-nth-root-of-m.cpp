58a9a7d8dd31e8b57e2500492642a370b7cd25c9
OMKAR KADAM <42216838+onkarkadam158@users.noreply.github.com>
2024-05-17 01:16:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Nth root of M/find-nth-root-of-m.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	
+    int bruteforce(int n,int m){
+	    for(int i=1;i<=m;i++){
+	        if(pow(i,n)==m) return i;
+	    }
+	    return -1;
+	}
+	int NthRoot(int n, int m)
+	{
+	    // Code here.
+	   // return bruteforce(n,m);
+	   int low=1,high=m;
+	   while(low<=high){
+	       int mid=(low+high)/2;
+	       if(pow(mid,n)==m) return mid;
+	       if(pow(mid,n)<m){
+	           low=mid+1;
+	       }
+	       else{
+	           high=mid-1;
+	       }
+	   }
+	   return -1;
+	}  
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		Solution ob;
+		int ans = ob.NthRoot(n, m);
+		cout << ans << "\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
